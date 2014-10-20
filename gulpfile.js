53b72ff86e32570b90fe138b1dc6e7f70d27e46f
gotoworks510 <yoshi@opdsgn.com>
2014-10-19 20:15:31 -0700

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+'use strict';
+var gulp = require('gulp'),
+	compass = require('gulp-compass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	plumber = require("gulp-plumber"),
+	webserver = require('gulp-webserver');
+
+// Directory
+var dir = {
+	current: 'htdocs',
+	css: 'htdocs/css',
+	sass: 'htdocs/sass',
+};
+
+// Typing "gulp" on the command line let all tasks run
+gulp.task('default', ['webserver', 'watch', 'compass']);
+
+gulp.task('webserver', function() {
+	gulp.src(dir.current)
+		.pipe(webserver({
+		host: 'localhost',
+		port: 9002,
+		livereload: true,
+		open: true
+	}));
+});
+
+// Call compass
+gulp.task('compass', function() {
+	gulp.src(dir.sass + '/*.scss')
+	.pipe(plumber())
+	.pipe(compass({
+		config_file: 'config.rb',
+		css: dir.css,
+		sass: dir.sass
+	}))
+	.pipe(autoprefixer('last 2 version'))
+	.pipe(gulp.dest(dir.css));
+});
+
+/// Watch
+gulp.task('watch', function() {
+	gulp.watch(dir.sass + '/*.scss',['compass']);
+});
